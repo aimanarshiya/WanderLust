5f4d412d9dc3ecc606eb421c6304af72534c1490
Aiman Arshiya <aimanarshiyaazeem@gmail.com>
2026-06-13 20:34:17 +0530

    corrected app.js and and user signup update

--- app.js
+++ app.js
@@ -6,9 +6,15 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,9 +24,6 @@ app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-const session = require("express-session");
-const flash = require("connect-flash");
-
 
 main()
   .then(() => console.log("Connected to Database"))
@@ -29,31 +32,38 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
-// expresss sessions
+
 const sessionOptions = {
-  secret :"mysupersecretcode",
-  resave : false,
-  saveUninitialized : true,
-  cookie : {
-    expires : Date.now() + 7*24*60*60*1000,
-    maxAge : 7*24*60*60*1000,
-    httpOnly : true
-  }
-}
+  secret: "mysupersecretcode",
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+  },
+};
 
 app.use(session(sessionOptions));
 app.use(flash());
 
-app.use((req,res,next)=> {
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
-})
+});
 
 app.get("/", (req, res) => {
   res.send("Hi, I am root Working here");
 });
 
+app.use("/", userRouter);
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 
