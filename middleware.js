024692af053db50d918abf54e0ac9e995ab6c3ca
Aiman Arshiya <aimanarshiyaazeem@gmail.com>
2026-06-13 22:06:25 +0530

    claude tiny changes made

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req, res, next) => {
+    console.log("isAuthenticated:", req.isAuthenticated()); // add this
     if (!req.isAuthenticated()) {
         req.flash("error", "You must be logged in!");
         return res.redirect("/login");
