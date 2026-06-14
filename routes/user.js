41da743358bac6d23ecf376e68377e76b56f5e11
Aiman Arshiya <aimanarshiyaazeem@gmail.com>
2026-06-14 14:13:23 +0530

    all i needad is --logout handle --navbar options accordingly session login and signup --direct login after signup --post login page to redirect wher we use else listings

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
 const passport = require("passport");
-
+const { saveRedirectUrl } = require("../middleware.js");
 
 // signup get route
 router.get("/signup", (req, res) => {
@@ -11,13 +11,16 @@ router.get("/signup", (req, res) => {
 });
 
 // signup post route
-router.post("/signup", wrapAsync(async (req, res) => {
+router.post("/signup", wrapAsync(async (req, res, next) => {
     try {
         let { username, email, password } = req.body;
         const newUser = new User({ username, email });
         const registeredUser = await User.register(newUser, password);
-        req.flash("success", "Welcome to WanderLust!");
-        res.redirect("/listings");
+        req.login(registeredUser, (err) => {
+            if (err) return next(err);
+            req.flash("success", "Welcome to WanderLust!");
+            res.redirect("/listings");
+        });
     } catch (e) {
         req.flash("error", e.message);
         res.redirect("/signup");
@@ -30,12 +33,22 @@ router.get("/login", (req, res) => {
 });
 
 // login post route
-router.post("/login", passport.authenticate("local", {
+router.post("/login", saveRedirectUrl, passport.authenticate("local", {
     failureRedirect: "/login",
     failureFlash: true,
 }), (req, res) => {
     req.flash("success", "Welcome back to WanderLust!");
-    res.redirect("/listings");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+});
+
+// logout route
+router.get("/logout", (req, res, next) => {
+    req.logout((err) => {
+        if (err) return next(err);
+        req.flash("success", "Logged out successfully!");
+        res.redirect("/listings");
+    });
 });
 
 module.exports = router;
