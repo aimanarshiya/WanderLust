92c0c724c5a59bf735844867aa7671835a40ef7f
Aiman Arshiya <aimanarshiyaazeem@gmail.com>
2026-06-15 19:41:08 +0530

    implementing router.route for listing.js and user.js

--- routes/listing.js
+++ routes/listing.js
@@ -4,12 +4,17 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
-router.get("/", wrapAsync(listingController.index));
+router.route("/")
+    .get(wrapAsync(listingController.index))
+    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+
 router.get("/new", isLoggedIn, listingController.renderNewForm);
-router.get("/:id", wrapAsync(listingController.showListing));
+
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
+
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
-router.post("/", isLoggedIn, validateListing, wrapAsync(listingController.createListing));
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing));
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router;
